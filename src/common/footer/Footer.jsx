9cc70a27d8293ef659856802c378266bc32a0486
Nkunja <munene01denis@gmail.com>
2023-07-24 16:15:53 +0300

    Final Commit

---
+++ src/common/footer/Footer.jsx
@@ -0,0 +1,59 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid2'>
+          <div className='box'>
+          <h1>Gamers HQ</h1>
+            <p>We are glad to have you our esteemed customer. Welcome you back for more appealing deals and items.</p>
+            <p>You can download our app from the links below;</p>
+            <div className='icon d_flex'>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-google-play'></i>
+                <span>Google Play</span>
+              </div>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-app-store-ios'></i>
+                <span>App Store</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Contact Us</h2>
+            <ul>
+              <li>Kitengela Town Opposite Kitengela Mall </li>
+              <li>Email: munene01denis@gmail.com</li>
+              <li>Phone: +254 796772263</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> 0796772263</label>
+            <i className='fa fa-envelope'></i>
+            <label> munene01denis@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Gamers HQ Shop</label>
+            <label>Need Help?</label>
+            <span>🏳️</span>
+            <label>ENG</label>
+            <span>🏳️</span>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
